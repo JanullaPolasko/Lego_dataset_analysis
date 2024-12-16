f73b79879bef8258a290c1ebac283da772641170
JanullaPolasko <jjpolaskova@gmail.com>
2024-12-16 21:41:13 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+# subprocess.run(["python", "create_tables.py"])
+# subprocess.run(["python", "insert_data.py"])
+
+# subprocess.run(["python", "cleaning.py"])
+
+
+subprocess.run(["python", "get_prices.py"])
+# subprocess.run(["python", "get_status.py"])
+# subprocess.run(["python", "get_pictures.py"])
+
+# subprocess.run(["python", "get_info.py"])
+
+# subprocess.run(["python", "data_to_excel.py"])
