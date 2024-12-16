454fe1cd8947ed1e0961e258efd7ac1726cd400d
JanullaPolasko <jjpolaskova@gmail.com>
2024-12-16 21:43:14 +0100

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import subprocess
 
+#create db and insert it with data from excel
+subprocess.run(["python", "create_tables.py"])
+subprocess.run(["python", "insert_data.py"])
 
-# subprocess.run(["python", "create_tables.py"])
-# subprocess.run(["python", "insert_data.py"])
+#cleaning (maybe not needed at this point cause excel is already cleaned
+subprocess.run(["python", "cleaning.py"])
 
-# subprocess.run(["python", "cleaning.py"])
-
-
+#crawling website about actual information
 subprocess.run(["python", "get_prices.py"])
-# subprocess.run(["python", "get_status.py"])
-# subprocess.run(["python", "get_pictures.py"])
+subprocess.run(["python", "get_status.py"])
+subprocess.run(["python", "get_pictures.py"])
 
-# subprocess.run(["python", "get_info.py"])
+subprocess.run(["python", "get_info.py"])
 
-# subprocess.run(["python", "data_to_excel.py"])
+#updating excel file
+subprocess.run(["python", "data_to_excel.py"])
